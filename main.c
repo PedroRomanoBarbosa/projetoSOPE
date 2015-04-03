818944e293fdd2e1633a588dbd38b0875d116dab
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2015-04-03 20:31:15 +0100

    Preparations update
    Added module to check if chapter files exist.

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 int main(int argc, char* args[]){
 	/*----------------------------------------*\
 					MAIN VARIABLES
 	\*----------------------------------------*/
-
-
-
-	/*----------------------------------------*\
-					PREPARATIONS
-	\*----------------------------------------*/
 	FILE* file;
 	char* c = "/words.txt";
 	int stringLength = strlen(args[1]);
 	char filePath[stringLength];
+
+	/*----------------------------------------*\
+					PREPARATIONS
+	\*----------------------------------------*/
+
+	/* check if words.txt exists and open it */
 	strcpy(filePath,args[1]);
 	strcat(filePath,c);
-
 	printf("---> Search for words.txt in %s\n\n",filePath);
 	file = fopen(filePath,"r");
 	if(file==NULL){
@@ -32,9 +32,33 @@ int main(int argc, char* args[]){
 		printf("---> The file words.txt was found! Preparing for indexing.\n\n");
 	}
 
+	/* check if other files exist */
+	int count = 0;
+	DIR* dir = opendir(args[1]);
+	struct dirent* dp;
+	printf("--> CHAPTER FILES TO INDEX:\n\n");
+	while((dp = readdir(dir)) != NULL){
+		if( strstr(dp->d_name,".txt") != NULL)
+			if( (strcmp(dp->d_name,"0_ReadMe.txt") != 0) 
+				&& (strcmp(dp->d_name, "words.txt") != 0) 
+				&& (strcmp(dp->d_name, "index.txt") != 0)){
+				printf("%s\n",dp->d_name);
+				count++;
+			}
+				
+	}
+	closedir(dir);
+	if(count==0){
+		printf("--> There aren't any chapter files to index.\n\n");
+		return 2;
+	}
+	printf("\n--> Number of chapter files in the directory: %d\n\n", count);
+
 	/*----------------------------------------*\
 				GET WORDS FROM WORDS.TXT
 	\*----------------------------------------*/
+
+	/* count the number of words in words.txt */
 	int nWords = 0;
 	int ch;
 	char buf[256];
@@ -44,12 +68,13 @@ int main(int argc, char* args[]){
 	}
 	if(nWords <= 0){
 		printf("---> You have no words in words.txt. Please insert them line by line.");
-		return 2;
+		return 3;
 		fclose(file);
 	}
 	fclose(file);
+
+	/* print all the words in words.tx */
 	file = fopen(filePath,"r");
-	printf("---> Number of words: %d\n\n",nWords);
 	char* words[nWords];
 	int index = 0;
 	printf("---> WORDS:\n\n");
@@ -59,6 +84,7 @@ int main(int argc, char* args[]){
     	index++;
   	}
   	printf("\n");
+  	printf("---> Number of words: %d\n\n",nWords);
 
 
 	/*----------------------------------------*\
